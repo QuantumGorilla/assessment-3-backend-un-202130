098c6a2ab568b348f297e5afe7a2776f92f4af6c
QuantumGorilla <manotasja@uninorte.edu.co>
2021-11-21 11:14:22 -0500

    feat: updating routers in app

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const ErrorSerializer = require('./src/serializers/BaseSerializer');
 const usersRouter = require('./src/routes/users');
+const tweetRouter = require('./src/routes/tweet');
+const commentRouter = require('./src/routes/comment');
 
 const app = express();
 
@@ -9,10 +11,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/users', usersRouter);
+app.use('/tweets', tweetRouter);
+app.use('/comments', commentRouter);
 
 app.use((req, res, next) => {
   res.status(404);
-  res.json(new ErrorSerializer('Not found', null));
+  res.json(new ErrorSerializer('Not found', null).toJSON());
 });
 
 app.use((err, req, res, next) => {
@@ -22,7 +26,7 @@ app.use((err, req, res, next) => {
   } = err;
 
   res.status(statusCode);
-  res.json(new ErrorSerializer(message, null));
+  res.json(new ErrorSerializer(message, null).toJSON());
 });
 
 module.exports = app;
